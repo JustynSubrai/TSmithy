e3f16efb1664e293f696ccc463b756c5815150cc
Justyn Subrai <justynskylarsubrai@gmail.com>
2022-11-22 19:43:10 -0800

    initial commit/boiler plate

---
+++ client/src/pages/Armory/Armory.jsx
@@ -0,0 +1,11 @@
+import './Armory.scss'
+
+function Armory() {
+  return (
+    <>
+      <div>This is Armory page</div>
+    </>
+  )
+}
+
+export default Armory
---
+++ client/src/pages/Community/Community.jsx
@@ -0,0 +1,11 @@
+import './Community.scss'
+
+function Community() {
+  return (
+    <>
+      <div>This is community page</div>
+    </>
+  )
+}
+
+export default Community
